6724a5257af5cd4367491e893d79c019fd395922
Hunter Arthur Gabbard <hunter.gabbard@dgx-1.dcs.ligo-wa.caltech.edu>
2017-04-21 07:12:53 -0700

    fixed weighting histogram transparency bug

--- pycbc_neural_network.py
+++ pycbc_neural_network.py
@@ -523,7 +523,7 @@ def main_plotter(prob_sort_noise, prob_sort_inj, run_num, out_dir, now, test_dat
     center_1 = (bins_1[:-1] + bins_1[1:]) / 2
     center_2 = (bins_2[:-1] + bins_2[1:]) / 2
     pl.bar(center_1, numpy_hist_1, log=True, label='background',color='b', alpha=0.6, align='center', width=width_1)
-    pl.bar(center_2, numpy_hist_2, log=True, label='injection', color='r', alpha=0.6, align='center', width=width_2)
+    pl.bar(center_2, numpy_hist_2, log=True, label='injection', color='r', alpha=0.4, align='center', width=width_2)
     pl.legend(frameon=True)
     pl.savefig('%s/run_%s/weights_hist.png' % (out_dir,now))
     pl.close()
