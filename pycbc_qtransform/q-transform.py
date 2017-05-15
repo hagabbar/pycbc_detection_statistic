410d721b8a60f7d127d23c57fbbc0f3434a83ae4
hagabbar <hunter.gabbard@gmail.com>
2017-05-15 14:37:21 +0200

    making plotting mechanism for qtransform

--- pycbc_qtransform/q-transform.py
+++ pycbc_qtransform/q-transform.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2017  Hunter A. Gabbard
-# Most of this has been shamelessly copied from Duncan Mcleod's GWPY qtransform.py script
+# Most of this is a port of Duncan Macleod's GWPY qtransform.py script
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -41,26 +41,70 @@ import urllib
 import datetime
 from scipy.interpolate import (interp2d, InterpolatedUnivariateSpline)
 from numpy import fft as npfft
-from matplotlib import use
-use('Agg')
-from matplotlib import pyplot as pl
 import argparse
 import datetime
 
+from matplotlib import use
+use('Agg')
+from matplotlib import pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
 from matplotlib.pyplot import specgram
 
 __author__ = 'Hunter Gabbard <hunter.gabbard@ligo.org>'
 __credits__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+
+def plotter(qplane, out_dir, now, frange, h1, sampling):
+    """
+    Parameters
+    """
+
+    # plot a spectrogram of the q-plane with the loudest normalized tile energy
+
+    dx = 0.001 #time resolution 
+    dy = 0.1 #frequency resolution
+    dur = int(len(h1)) / sampling #duration of analysis period in seconds
+
+    # generate 2 2d grids for the x & y bounds
+    y, x = np.mgrid[slice(frange[0], frange[1], dy), # Should replace zero/dur with start/end times
+                    slice(0, dur, dx)]
+    z = qplane.T
+
+    # x and y are bounds, so z should be the value *inside* those bounds.
+    # Therefore, remove the last value from the z array.
+    #z = z[:-1, :-1]
+    levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
+
+
+    # pick the desired colormap, sensible levels, and define a normalization
+    # instance which takes data values and translates those into levels.
+    cmap = plt.get_cmap('PiYG')
+    norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+
+    fig, (ax0, ax1) = plt.subplots(nrows=2)
+
+    im = ax0.pcolormesh(x, y, z, cmap=cmap, norm=norm)
+    fig.colorbar(im, ax=ax0)
+    ax0.set_title('pcolormesh with levels')
+
+
+    # adjust spacing between subplots so `ax1` title and `ax0` tick labels
+    # don't overlap
+    fig.tight_layout()
+
+    plt.show()
 
 def Qplane(qplane_tile_dict, h1, sampling, normalized, out_dir, now, frange):
     """
     Parameters
     """
 
-    #perform q-transform on each tile for each q-plane and pick out the tile that has the largest normalized energy 
-    #store q-transforms of each tile in a dict
+    # perform q-transform on each tile for each q-plane and pick out the tile that has the largest normalized energy 
+    # store q-transforms of each tile in a dict
     qplane_qtrans_dict = {}
     tres=.001
+    fres = .1
+    dur = int(len(h1)) / sampling
 
     max_norm_energy = [] 
     for i, key in enumerate(qplane_tile_dict):
@@ -68,8 +112,6 @@ def Qplane(qplane_tile_dict, h1, sampling, normalized, out_dir, now, frange):
         norm_energies_lst=[]
         for tile in qplane_tile_dict[key]:
             norm_energies = qtransform(h1, tile[1], tile[0], sampling, normalized)
-            print norm_energies
-            sys.exit()
             norm_energies_lst.append(norm_energies)
             if i == 0:
                 max_norm_energy.append(max(norm_energies))
@@ -79,51 +121,55 @@ def Qplane(qplane_tile_dict, h1, sampling, normalized, out_dir, now, frange):
                 max_norm_energy[0] = max(norm_energies)
                 max_norm_energy[1] = tile
                 max_norm_energy[2] = key
+                max_norm_energy[3] = norm_energies
         qplane_qtrans_dict[key] = np.array(norm_energies_lst)
 
-
     # build regular Spectrogram from peak-Q data by interpolating each
-    #If you get lost, refer to https://github.com/gwpy/gwpy/blob/44d8d6381d2d03fb5d0b7d5484885512f9b841b1/gwpy/timeseries/timeseries.py
-    #Line 1780
+    # if you get lost, refer to https://github.com/gwpy/gwpy/blob/44d8d6381d2d03fb5d0b7d5484885512f9b841b1/gwpy/timeseries/timeseries.py
+    # line 1780
     # (Q, frequency) `TimeSeries` to have the same time resolution
-    nx = int(abs(Segment(*outseg)) / tres)
-    ny = frequencies.size
-    out = Spectrogram(np.zeros((nx, ny)), x0=outseg[0], dx=tres,
-                      frequencies=frequencies)
-    out._yindex = type(out.y0)(frequencies, out.y0.unit)
-    # record Q in output
-    out.q = peakq
-    # interpolate rows
-    for i, row in enumerate(norms)
-        row = row.crop(*outseg)
-        interp = InterpolatedUnivariateSpline(row.times.value, row.value)
-        out[:, i] = interp(out.times.value)
+
+
+    # record peak q calculate above and q-transform output for peak q
+    peakq = max_norm_energy[1][1]
+    norm = qplane_qtrans_dict[max_norm_energy[2]]     
+    
+    #create time array
+    time_array = np.zeros(int(dur / tres))
+    #for idx, i in enumerate(time_array): 
+    #    time_array[idx] = idx
+
+    # interpolate rows for better time resolution
+    interp_norm = []
+    for i, row in enumerate(norm):
+        row_arry = np.zeros(len(row))
+        for idx, i in enumerate(row_arry): 
+            row_arry[idx] = idx
+        interp = InterpolatedUnivariateSpline(row_arry, row)
+        interp_norm.append(interp(time_array))
+
 
     # then interpolate the spectrogram to increase the frequency resolution
-    # --- this is done because duncan doesn't like interpolated images
-    #     because they don't support log scaling
     if fres is None:  # unless user tells us not to
-        return out
+        return inter_norm
     else:
-        interp = interp2d(out.times.value, frequencies, out.value.T,
+        # initialize some variables
+        time_array = np.zeros(int(dur / tres))
+        for idx, i in enumerate(time_array): 
+            time_array[idx] = idx
+        time_null_array = np.zeros(int(dur / tres))
+        frequencies = []
+        for idx, i in enumerate(qplane_tile_dict[max_norm_energy[2]]):
+            frequencies.append(i[0])
+
+        # 2-D interpolation 
+        interp = interp2d(time_array, frequencies, interp_norm,
                           kind='cubic')
-        f2 = numpy.arange(planes.frange[0], planes.frange[1], fres)
-        new = Spectrogram(interp(out.times.value, f2 + fres/2.).T,
-                          x0=outseg[0], dx=tres,
-                          f0=planes.frange[0], df=fres)
-        new.q = peakq
-        return new
+        f2 = np.arange(int(frange[0]), int(frange[1]), fres)
 
-
-    #Performing q-transform on tile with largest normalised tile energy 
-    #final_q = qtransform(h1, max_norm_energy[1][1], max_norm_energy[1][0], sampling, normalized)
-
-    #Plot spectrogram of result
-    #pl.figure()
-    #Pxx, freqs, bins, im = specgram(final_q)
-    #pl.savefig('%s/run_%s/qplot.png' % (out_dir, now))
-    #pl.close()
-    
+        # this is the last part you need to fix
+        out = interp(time_null_array, f2)
+    return out 
 
 def qtiling(h1, qrange, frange, sampling, normalized, mismatch):
     """
@@ -331,7 +377,7 @@ def main():
     url = "https://losc.ligo.org/s/events/GW150914/" + fname
     urllib.urlretrieve(url, filename=fname)
     h1 = read_frame('H-H1_LOSC_4_V2-1126259446-32.gwf', 'H1:LOSC-STRAIN')
-    h1 = TimeSeries(np.random.normal(size=256*4096), delta_t = 1. / sampling)
+    h1 = TimeSeries(np.random.normal(size=64*4096), delta_t = 1. / sampling)
     h1 = highpass_fir(h1, 15, 8)
 
     # Calculate the noise spectrum
@@ -341,7 +387,10 @@ def main():
     Qbase, frange = qtiling(h1, qrange, frange, sampling, normalized, mismatch)
 
     #Choose Q-plane and plot
-    Qplane(Qbase, h1, sampling, normalized, out_dir, now, frange)
+    qplane = Qplane(Qbase, h1, sampling, normalized, out_dir, now, frange)
+
+    #Plot spectrogram
+    plotter(qplane, out_dir, now, frange, h1, sampling)
 
 if __name__ == '__main__':
     main()
